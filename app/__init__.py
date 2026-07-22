2621e0c69df9e41244e8d2d57bb8e4c1ab75bed6
adindanrsn <brightestar15@gmail.com>
2026-07-22 23:14:06 +0700

    Initialize Flask application structure

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+from config import Config
+from app.extensions import db, login_manager, migrate
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    migrate.init_app(app, db)
+
+    @app.route("/")
+    def home():
+        return "<h1>Hello, UMKM!</h1>"
+
+    return app
---
+++ run.py
@@ -0,0 +1,7 @@
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    print("Flask akan dijalankan")
+    app.run(debug=True)
